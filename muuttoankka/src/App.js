82fec3bd8ab7e4190fcbe04aeda46d8dffa846b2
NorthernLion <joppetaja@gmail.com>
2018-02-18 17:10:18 +0200

    All functions working

--- muuttoankka/src/App.js
+++ muuttoankka/src/App.js
@@ -5,21 +5,52 @@ import SightingsList from './components/SightingsList'
 import SightingsForm from './components/SightingsForm'
 import Togglable from './components/Togglable'
 import Navigationbar from './components/Navigationbar'
+import { Button } from 'react-bootstrap'
 class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
       species: [],
+      orderedSightings: [],
       sightings: [],
       description: '',
       specie: '',
-      count: 1
+      count: 1,
+      inOrder: false
     }
+  }
+
+  componentWillMount() {
+    sightingsService.getAll().then(sightings => {
+      this.setState({ sightings, orderedSightings: sightings })
+    })
+    speciesService.getAll().then(species => {
+      this.setState({ species })
+    })
+    this.handleOrdering
   }
 
   handleChange = (event) => {
     this.setState({ [event.target.name]: event.target.value })
   }
+
+  handleOrdering = () => {
+    const timeOrder = this.state.sightings.sort((a, b) => new Date(b.dateTime) - new Date(a.dateTime))
+    if (this.state.inOrder) {
+      this.setState({
+        orderedSightings: timeOrder,
+        inOrder: false
+      })
+      
+    } else {
+      this.setState({ 
+        orderedSightings: timeOrder.reverse(),
+        inOrder: true
+      })
+    }
+  }
+
+
 
   handlePost = async (event) => {
     event.preventDefault()
@@ -37,7 +68,8 @@ class App extends React.Component {
       description: '',
       specie: '',
       count: 1,
-      sightings: this.state.sightings.concat(sighting)
+      sightings: this.state.sightings.concat(sighting),
+      orderedSightings: this.state.sightings.concat(sighting)
     })
     } else {
       console.log('Wrong information')
@@ -47,15 +79,6 @@ class App extends React.Component {
   validState = () => {
     const comparedSpecies = this.state.species.filter(spec => spec.name.includes(this.state.specie))
     return (this.state.description && this.state.count > 0 && comparedSpecies.length > 0 && this.state.specie)
-  }
-
-  componentWillMount () {
-    sightingsService.getAll().then(sightings => {
-      this.setState({ sightings })
-    })
-    speciesService.getAll().then(species => {
-      this.setState({ species })
-    })
   }
 
   render () {
@@ -73,9 +96,10 @@ class App extends React.Component {
             count={this.state.count}
           />
         </Togglable>
-
         <p></p>
-        <SightingsList sightings={this.state.sightings} />      
+        <Button type="submit" onClick={this.handleOrdering}>Sort by Date</Button>
+        <p></p>
+        <SightingsList sightings={this.state.orderedSightings} />      
 
 
       </div>
